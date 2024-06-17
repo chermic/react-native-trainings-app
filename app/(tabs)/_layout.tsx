2fd1222f1758a5f6bf6ed8e094aa546c2d9aca9d
Mikhail Chernyshenko <chernyshenkomd@intech.rshb.ru>
2024-06-17 10:43:39 +0300

    feat(cause-page): add cause-page base layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,6 +15,15 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
+        name="cause"
+        options={{
+          title: 'Cause',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
---
+++ app/(tabs)/cause.tsx
@@ -0,0 +1,12 @@
+import { ThemedView } from "@/components/ThemedView";
+import { SafeAreaViewComponent, Text } from "react-native";
+
+export default function Cause() {
+  return (
+    <ThemedView>
+      <SafeAreaViewComponent>
+        <Text>Hello world</Text>
+      </SafeAreaViewComponent>
+    </ThemedView>
+  );
+}
